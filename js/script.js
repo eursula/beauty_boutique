861e7a542b655bbeed8ee667d7af87e7779da545
Eursula Hicks <eursula.hicks@gmail.com>
2014-12-15 16:46:56 +1300

    slider fixing

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
-/*$(document).ready(function ($) {
+$(document).ready(function ($) {
             var options = {
 
-                $AutoPlay: true,                                   //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
-                $DragOrientation: 3                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
+                $AutoPlay: true,          //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
+                $DragOrientation: 3       //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
             };
 
             var jssor_slider1 = new $JssorSlider$("slider1_container", options);
-        });*/
+        });
 
+
+
+/* function to toggle between icons on mobile device
+   to show different hidden menus*/
 jQuery(function(){
 
 	var $ = jQuery;
@@ -35,34 +39,27 @@ jQuery(function(){
 
     });
 
+	// script for responsive tabs 
 	$('.verticalTab').easyResponsiveTabs({
 		type: 'vertical',
 		width: 'auto',
 		fit: true
 	});
-
-	$(document).ready(function() {
-		$(".show").fancybox({
-			'titleShow'	 : 'false',
-			'transitionIn'	  : 'fade',
-			'transitionOut'	 : 'fade'
-		});
 	
-
-
+	// MailChimp script for newsletter subscription
 	(function($) {
 		window.fnames = new Array(); 
 		window.ftypes = new Array();
 		fnames[0]='EMAIL';
 		ftypes[0]='email';
-		fnames[1]='FNAME';
-		ftypes[1]='text';
 	}(jQuery));
 
 	var $mcj = jQuery.noConflict(true);
 
+ 	
 
-});
+
+
 
 
 
--- page-with-name.php
+++ page-with-name.php
@@ -1,19 +1,29 @@
 <?php /* Template Name: Page with Name */ ?>
 
 <?php get_header(); ?>
-	<!--<div class="face-pic">
-		<div id="slider1_container">
+	<div class="face-pic">
+		<div id="slider1_container" style="position: fixed; top: 0px; left: 0px; width: 1400px; height: 650px;">
 	        <!-- Slides Container -->
-	        <!--<div u="slides" style="position: fixed; left: 0px; top: 0px; width: 1100px; height: 550px;
+	        <div u="slides" style="position: fixed; left: 50%; top: 0px; margin-left: -700px; width: 1400px; height: 650px;
 	            overflow: hidden;">
-	            <div><img u="image" src="wp-content/themes/my-theme/css/img/beauty-large.png" /></div>
-	            <div><img u="image" src="wp-content/themes/my-theme/css/img/beauty-therapy.jpg" /></div>
+	            <div>
+	            	<img u="image" src="wp-content/themes/my-theme/css/img/beauty-large.png" />
+	            </div>
+	            <div><img u="image" src="wp-content/themes/my-theme/css/img/stone-spa.jpg" /></div>
+	            <div><img u="image" src="wp-content/themes/my-theme/css/img/beauty-therapy.jpg" /></div> 
 	        </div>
 	    </div>
 	</div> <!-- Jssor Slider End -->
-	<div class="face-pic">
+	<!--<div class="face-pic">
 		<img src="wp-content/themes/my-theme/css/img/beauty-large.png" alt="">
-	</div>
+	</div>-->
+	<!--<div class="face-pic" style="position:fixed">
+		<div id="slides">
+	    	<img src="wp-content/themes/my-theme/css/img/beauty-large.png" alt="slider1">
+	      	<img src="wp-content/themes/my-theme/css/img/stone-spa.jpg" alt="slider2">
+	      	<img src="wp-content/themes/my-theme/css/img/beauty-therapy.jpg" alt="slider3">
+	    </div>
+	</div>-->
 	<div id="content">
 		<div class="container flex flex-column flex-a-center">
 			<div class="title">
